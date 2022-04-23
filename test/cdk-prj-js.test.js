4275790a1e06f4512ea3577f04b91ddfc68807ac
Rodrigo Chagas Teixeira da Silva <rcts@br.ibm.com>
2022-04-23 18:44:13 -0300

    Initial commit

---
+++ test/cdk-prj-js.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkPrjJs = require('../lib/cdk-prj-js-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-prj-js-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkPrjJs.CdkPrjJsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
